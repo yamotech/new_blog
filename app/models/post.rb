13b4af2eb1219cc7d22da2d9ea996b08f52d081d
yamotech <nothings.2c9@gmail.com>
2016-07-05 00:31:33 +0900

    バリデーション

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ActiveRecord::Base
   scope :find_newest_article, -> { order(created_at: :desc).limit(5) }
+
+  validates :title,     presence: :true, length: { maximum: 50 }
+  validates :category,  presence: :true, length: { maximum: 50 }
+  validates :body,      presence: :true, length: { maximum: 1000 }
 end
