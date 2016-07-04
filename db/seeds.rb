7db0c8ca6216e667962bfb4b0dccc46e0f2f779c
yamotech <nothings.2c9@gmail.com>
2016-07-04 15:05:38 +0900

    seedデータの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+post = ["この投稿はスパルタキャンプ in hachimantai Ruby on Rails用のサンプルアプリです", "年齢不問、経験不問、職歴不問。プログラミングを学びたいという”強い思い”を持っている人が対象のプログラミング学習イベントです。", "スパルタキャンプでは、本や教科書で学ぶのとは異なり分からないことがあったらすぐに質問することができる環境で進めていくことができるので、挫折することがありません。"]
+
+category = ["Rails", "雑感", "未分類"]
+
+30.times do |index|
+  Post.create(title: "#{index}回目", body: "#{index}回目の投稿です。#{post.sample}", category: "#{category.sample}")
+end
