9747c790ed884d71bedc978584cf1c30519af375
yamotech <nothings.2c9@gmail.com>
2016-07-05 23:18:23 +0900

    バリデーションの変更

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   scope :find_newest_article, -> { order(created_at: :desc).limit(5) }
 
-  validates :title,     presence: :true, length: { maximum: 50 }
-  validates :category,  presence: :true, length: { maximum: 50 }
-  validates :body,      presence: :true, length: { maximum: 1000 }
+  validates :title,        presence: :true, length: { maximum: 50 }
+  validates :category_id,  presence: :true
+  validates :body,         presence: :true, length: { maximum: 1000 }
 end
