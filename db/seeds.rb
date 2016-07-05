83ab2270c19ecc9a9ce31a43b685edcb1f2ea9d7
yamotech <nothings.2c9@gmail.com>
2016-07-05 23:40:04 +0900

    seedファイルの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,10 +8,13 @@
 
 post = ["この投稿はスパルタキャンプ in hachimantai Ruby on Rails用のサンプルアプリです", "年齢不問、経験不問、職歴不問。プログラミングを学びたいという”強い思い”を持っている人が対象のプログラミング学習イベントです。", "スパルタキャンプでは、本や教科書で学ぶのとは異なり分からないことがあったらすぐに質問することができる環境で進めていくことができるので、挫折することがありません。"]
 
+Category.create(title: '日々')
+Category.create(title: '雑感')
+Category.create(title: '未分類')
+
 Author.create(name: "八幡太郎", description: "八幡平の太郎です。", image_url: "http://heaaart.com/heartadmin/wp-content/uploads/2015/04/49716-1.png", email: "test@gmail.com", password: "password")
 
-category = ["Rails", "雑感", "未分類"]
 
 30.times do |index|
-  Post.create(title: "#{index}回目", body: "#{index}回目の投稿です。#{post.sample}", category: "#{category.sample}")
+  Post.create(title: "#{index}回目", body: "#{index}回目の投稿です。#{post.sample}", category_id: rand(3) + 1 )
 end
