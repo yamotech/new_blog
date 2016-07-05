cb3ec8a5f9eeb00f6c4f1c2db1b2222989d59095
yamotech <nothings.2c9@gmail.com>
2016-07-05 23:12:29 +0900

    サイドバーにAuthor情報を表示

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,8 @@
 
 post = ["この投稿はスパルタキャンプ in hachimantai Ruby on Rails用のサンプルアプリです", "年齢不問、経験不問、職歴不問。プログラミングを学びたいという”強い思い”を持っている人が対象のプログラミング学習イベントです。", "スパルタキャンプでは、本や教科書で学ぶのとは異なり分からないことがあったらすぐに質問することができる環境で進めていくことができるので、挫折することがありません。"]
 
+Author.create(name: "八幡太郎", description: "八幡平の太郎です。", image_url: "http://heaaart.com/heartadmin/wp-content/uploads/2015/04/49716-1.png", email: "test@gmail.com", password: "password")
+
 category = ["Rails", "雑感", "未分類"]
 
 30.times do |index|
