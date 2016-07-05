0ff90fba05289cfe79349c8aa50437ae105db598
yamotech <nothings.2c9@gmail.com>
2016-07-05 12:47:44 +0900

    edit,update実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,9 +22,16 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post, notice: '更新に成功しました'
+    else
+      render 'edit'
+    end
   end
 
   def destroy
