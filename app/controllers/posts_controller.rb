553b73f78a5d9f3c319ec9577ca9b05d44d955af
yamotech <nothings.2c9@gmail.com>
2016-07-06 00:03:05 +0900

    ページネーション

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :authenticate_author!, only: [ :new, :create, :edit, :update, :destroy ]
 
   def index
-    @posts = Post.all.order(created_at: :desc)
+    @posts = Post.order(created_at: :desc).page(params[:page]).per(10)
     @new_posts = Post.find_newest_article
   end
 
