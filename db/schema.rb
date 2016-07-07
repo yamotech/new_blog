1cad60800bfaca45cf07c12737b6d793b38ac8d5
yamotech <nothings.2c9@gmail.com>
2016-07-07 16:28:24 +0900

    コメント投稿機能 モデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160705141506) do
+ActiveRecord::Schema.define(version: 20160707071612) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -39,6 +39,15 @@ ActiveRecord::Schema.define(version: 20160705141506) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "body"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["post_id"], name: "index_comments_on_post_id"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
